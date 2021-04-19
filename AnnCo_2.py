35d5b82b6379a1c95fdb950a7ce04e17cbd7166c
v-girak <provetal07@gmail.com>
2021-04-19 21:26:42 +0300

    Added constructor from .eaf file contents to Annotation class

--- AnnCo_2.py
+++ AnnCo_2.py
@@ -80,6 +80,96 @@ class Annotation:
 
         return cls(tiers, duration)
 
+    @classmethod
+    def from_eaf(cls, contents):
+        """Creates annotation from .eaf contents."""
+
+        ann_doc = contents.getroot()
+        duration = cls.get_duration(ann_doc)
+
+        align_anns = ann_doc.findall('.//ALIGNABLE_ANNOTATION')
+        ann_doc = cls.set_align_ann_times(ann_doc, align_anns)
+        ann_doc = cls.set_ref_ann_times(ann_doc, align_anns)
+
+        tiers = cls.get_tiers(ann_doc)
+
+        return cls(tiers, duration)
+
+    @staticmethod
+    def set_ref_ann_times(root, annotations) -> ET.Element:
+        """Assigns time boundaries to referring annotations."""
+
+        for ann in annotations:
+            for t in root.findall('TIER'):
+                ref_anns = t.findall(
+                    f".//*[@ANNOTATION_REF='{ann.get('ANNOTATION_ID')}']"
+                )
+                if ref_anns:
+                    ref_dur = (
+                        (ann.get('TIME_SLOT_REF2') - ann.get('TIME_SLOT_REF1'))
+                        / len(ref_anns)
+                    )
+                    ref_time = ann.get('TIME_SLOT_REF1')
+
+                    for ref in ref_anns:
+                        ref.set('TIME_SLOT_REF1', ref_time)
+                        ref_time += ref_len
+                        ref.set("TIME_SLOT_REF2", ref_time)
+
+                    set_ref_ann_times(root, ref_anns)
+
+        return root
+
+    @staticmethod
+    def get_duration(root) -> float:
+        """Gets annotation duration from .eaf file root.
+        
+        Extracts duration from a media file. If media file is absent,
+        sets value of the last time slot as duration.
+        """
+
+        try:
+            wav_path = root.find('HEADER/MEDIA_DESCRIPTOR').get('MEDIA_URL')
+            with wave.open(wav_path[8:], 'rb') as wav:
+                duration = wav.getnframes() / wav.getframerate()
+        except:
+            duration = int(root.find('TIME_ORDER')[-1].get('TIME_VALUE')) / 1000
+
+        return duration
+
+    @staticmethod
+    def set_align_ann_times(root, annotations) -> ET.Element:
+        """Replaces alignable annotations' time references with times."""
+
+        for ann in annotations:
+            for slot in root.find('TIME_ORDER'):
+
+                if ann.get('TIME_SLOT_REF1') == slot.get('TIME_SLOT_ID'):
+                    ann.set('TIME_SLOT_REF1', int(slot.get('TIME_VALUE')) / 1000)
+
+                if ann.get('TIME_SLOT_REF2') == slot.get('TIME_SLOT_ID'):
+                    ann.set('TIME_SLOT_REF2', int(slot.get('TIME_VALUE')) / 1000)
+
+        return root
+
+    @staticmethod
+    def get_tiers(root) -> list:
+        """Returns tiers and their intervals from .eaf file root."""
+
+        tiers = []
+        for t in root.findall('TIER'):
+            intervals = []
+
+            for ann in t.findall('ANNOTATION/*'):
+                intervals.append(
+                    Interval(ann.get('TIME_SLOT_REF1'),
+                             ann.get('TIME_SLOT_REF1'),
+                             ann.find("*").text)
+                )
+            tiers.append(Tier(t.get('TIER_ID'), intervals))
+
+        return tiers
+
 
 class Converter:
     """Represents AnnCo interface."""

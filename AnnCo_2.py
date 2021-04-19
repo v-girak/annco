86d0411f3b51d237145c30d6a71c2581ae3c8db2
v-girak <provetal07@gmail.com>
2021-04-19 15:50:53 +0300

    Created AnnCo_2 and Ideas

---
+++ AnnCo_2.py
@@ -0,0 +1,23 @@
+import re
+import wave
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as MD
+import tkinter as tk
+
+from tkinter import ttk, messagebox
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+
+
+class Interval:
+    """Represents annotation interval."""
+
+    def __init__(self, start: float, end: float, text):
+        self.start = start
+        self.end = end
+        self.text = text
+    
+    def __repr__(self):
+        return f'Interval({self.start}, {self.end}, {self.text})'
+    
+    def __str__(self):
+        return self.text
---
+++ Ideas.md
@@ -0,0 +1,11 @@
+# Ideas for AnnCo 2.0
+
+***
+
+## Refactoring
+- Compile TextGrid search patterns.
+- Get rid of minidom.
+- Move `ENCOD_MSG` to `Application` class.
+- Make `Annotation` and `Tier` classes iterable.
+- Try replacing index with re pattern in `wav_path`.
+- Store Tier and Annotation elements?
